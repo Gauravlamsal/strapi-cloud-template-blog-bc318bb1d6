76732ec81e53d4acbb0e56caaedd1ce451928a22
Gauravlamsal <112841520+Gauravlamsal@users.noreply.github.com>
2026-07-05 00:24:56 -0500

    Configure HitJuwa Strapi CORS

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,19 @@
-module.exports = [
+module.exports = ({ env }) => [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: env.array('CORS_ORIGIN', [
+        'https://hitjuwa.com',
+        'https://www.hitjuwa.com',
+        'http://127.0.0.1:1337',
+        'http://localhost:1337',
+      ]),
+      headers: '*',
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
