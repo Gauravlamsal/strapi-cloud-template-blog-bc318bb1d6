0bff710aca8f4db3c95e01c53d865af236165a47
Gauravlamsal <112841520+Gauravlamsal@users.noreply.github.com>
2026-07-05 00:24:47 -0500

    Configure HitJuwa Strapi runtime

--- config/server.js
+++ config/server.js
@@ -1,10 +1,16 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
+module.exports = ({ env }) => {
+  const publicUrl = env('PUBLIC_URL', '');
+
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    url: publicUrl || undefined,
+    proxy: env.bool('STRAPI_PROXY', false),
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+    webhooks: {
+      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
+  };
+};
